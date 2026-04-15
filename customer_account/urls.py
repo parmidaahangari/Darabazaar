6c4dbe3dc1cb843f94a8bd5292bbfd00cc806d58
parmidaahangari <parmida.ahangari@gmail.com>
2026-04-15 23:10:03 +0330

    personal information...

--- customer_account/urls.py
+++ customer_account/urls.py
@@ -3,6 +3,7 @@ from . import views
  
 urlpatterns = [
     path('dashboard/', views.CustomerAccountView.as_view(), name='dashboard'),
+    path('information/', views.CustomerAccountInfoView.as_view(), name='personal_information'),
 
     
 ]
--- customer_account/views.py
+++ customer_account/views.py
@@ -1,8 +1,38 @@
 from django.shortcuts import render, redirect
 from django.views import View
+from .forms import *
+from .models import *
 
 class CustomerAccountView(View):
     
     def get(self, request):
         return render(request, 'customer_account/CustomerAccount.html', {          
+        })
+    
+
+class CustomerAccountInfoView(View):
+    
+    def get(self, request):
+        return render(request, 'customer_account/CustomerInfo.html', {          
+        })
+
+    def post(self, request):
+        form = UserForm(request.POST)
+
+        if form.is_valid():
+            user = User()
+            user.username = form.cleaned_data['username']
+            user.phone = form.cleaned_data['phone']
+            user.email = form.cleaned_data['email']
+
+            user.set_password(form.cleaned_data['password'])  
+            user.save()
+            return redirect("/dashboard/")
+
+
+
+        return render(request,'customer_account/CustomerInfo.html',{
+            "form": UserForm(),
+
+            "success": True
         })
